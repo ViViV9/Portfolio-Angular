1a141a3637735a374f198719ab663840b31f3923
ViViV9 <vivianaverachtert@gmail.com>
2023-02-07 17:15:06 -0300

    cargando json

--- src/app/educacion/educacion.component.ts
+++ src/app/educacion/educacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from '../servicios/portfolio.service';
 
 @Component({
   selector: 'app-educacion',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educacion.component.css']
 })
 export class EducacionComponent implements OnInit {
+  id: string= '';
+  curso: string= '';
+  titulo: string= '';
+  institucion: string= '';
+  inicio: string= '';
+  fin: string= '';
+  logo: string= '';
+  nombre_curso: string= '';
+  
 
-  constructor() { }
+  constructor(private porfolioService: PortfolioService) { }
   usuarioLogueado= true
   ngOnInit(): void {
+    this.porfolioService.getDatos().subscribe(datos => {
+      console.log(datos);
+      this.id=datos.id;
+      this.curso=datos.curso;
+      this.titulo=datos.titulo;
+      this.institucion=datos.institucion;
+      this.inicio=datos.inicio;
+      this.fin=datos.fin;
+      this.logo=datos.logo;
+      this.nombre_curso=datos.nombre_curso;
+    });
   }
-
 }
